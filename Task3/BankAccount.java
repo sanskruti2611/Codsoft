54b9b1eea44e84989b856f05d247f2b51335a36c
moonlight <116077763+sanskruti2611@users.noreply.github.com>
2024-03-24 12:54:58 +0530

    Add files via upload

---
+++ Task3/BankAccount.java
@@ -0,0 +1,30 @@
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: Rs" + amount);
+        } else {
+            System.out.println("Invalid amount for deposit.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: Rs" + amount);
+        } else {
+            System.out.println("Invalid amount for withdrawal or insufficient balance.");
+        }
+    }
+}
